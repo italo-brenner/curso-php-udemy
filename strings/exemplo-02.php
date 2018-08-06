b1c76d245890ef16dda81f57c19cc543db60f2a5
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2018-08-06 16:38:19 -0300

    Ate a aula for

---
+++ strings/exemplo-02.php
@@ -0,0 +1,21 @@
+<?php
+
+$nome = "joão rangel";
+
+$nome = strtoupper($nome);
+
+echo $nome."<br>";
+
+$nome = strtolower($nome);
+
+echo $nome."<br>";
+
+$nome = ucfirst($nome);
+
+echo $nome."<br>";
+
+$nome = ucwords($nome);
+
+echo $nome."<br>";
+
+?>
---
+++ variaveis/exemplo-04.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome = (int) $_GET["a"];
+
+//var_dump($nome);
+
+$ip = $_SERVER;
+
+echo $ip;
+
+?>

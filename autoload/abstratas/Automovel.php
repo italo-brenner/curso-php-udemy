2aef8e4d246f567559e8fad6172731cf5dcf7f56
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2018-08-08 16:53:09 -0300

    Autoload

---
+++ autoload/abstratas/Automovel.php
@@ -0,0 +1,19 @@
+<?php
+
+abstract class Automovel implements Veiculo {
+
+    public function acelerar($velocidade) {
+        echo "O veículo acelerou até $velocidade km/h";
+    }
+
+    public function frenar($velocidade) {
+        echo "O veículo frenou até $velocidade km/h";
+    }
+
+    public function trocarMarcha($marcha) {
+        echo "O veículo engatou a marcha $marcha";
+    }
+
+}
+
+?>

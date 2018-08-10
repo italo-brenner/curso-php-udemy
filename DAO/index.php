731df79121b36d1cfc2a9f1b2150c83b8483a004
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2018-08-10 14:16:59 -0300

    Comando DELETE

--- DAO/index.php
+++ DAO/index.php
@@ -25,9 +25,16 @@ require_once("config.php");
 //$aluno->insert();
 //echo $aluno;
 
+// Alterar um usuário
+//$usuario = new Usuario();
+//$usuario->loadById(6);
+//$usuario->update("professor", "!@#$");
+//echo $usuario;
+
+// Deletar usuario
 $usuario = new Usuario();
 $usuario->loadById(6);
-$usuario->update("professor", "!@#$");
+$usuario->delete();
 echo $usuario;
 
 ?>

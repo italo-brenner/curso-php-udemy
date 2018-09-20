8d1df61d89008f2fa41b3651b5d41e9a2f281972
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2018-09-20 10:52:07 -0300

    Carrinho calculo de frete

--- ecommerce/vendor/hcodebr/php-classes/src/Model/Cart.php
+++ ecommerce/vendor/hcodebr/php-classes/src/Model/Cart.php
@@ -9,6 +9,7 @@ use \Hcode\Model\User;
 class Cart extends Model {
 
 	const SESSION = "Cart";
+	const SESSION_ERROR = "CartError";
 
 	public static function getFromSession()
 	{
@@ -118,6 +119,8 @@ class Cart extends Model {
 			':idcart'=>$this->getidcart(),
 			':idproduct'=>$product->getidproduct()
 		]);
+
+		$this->getCalculateTotal();
 	}
 
 	public function removeProduct(Product $product, $all = false)
@@ -148,6 +151,8 @@ class Cart extends Model {
 			]);
 		}
 
+		$this->getCalculateTotal();
+
 	}
 
 	public function getProducts()
@@ -170,6 +175,131 @@ class Cart extends Model {
 		", [
 			":idcart"=>$this->getidcart()
 		]));
+	}
+
+	public function getProductsTotals()
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			select 
+				sum(vlprice) as vlprice,
+				sum(vlwidth) as vlwidth,
+				sum(vlheight) as vlheight,
+				sum(vllength) as vllength,
+				sum(vlweight) as vlweight,
+				count(1) as nrqtd
+			from tb_products a
+			inner join tb_cartsproducts b on a.idproduct = b.idproduct
+			where b.idcart = :idcart
+			and b.dtremoved is null
+		", [
+			":idcart"=>$this->getidcart()
+		]);
+
+		if (count($results) > 0) {
+			return $results[0];
+		} else {
+			return [];
+		}
+
+	}
+
+	public function setFreight($nrzipcode)
+	{
+		$nrzipcode = str_replace('-', '', $nrzipcode);
+
+		$totals = $this->getProductsTotals();
+
+		if ($totals['nrqtd'] > 0) {
+
+			if ($totals['vlheight'] < 2) {
+				$totals['vlheight'] = 2;
+			} else if ($totals['vllength'] < 16) {
+				$totals['vllength'] = 16;
+			}
+			$qs = http_build_query([
+				'nCdEmpresa'=>'',
+				'sDsSenha'=>'',
+				'nCdServico'=>'40010',
+				'sCepOrigem'=>'09853120',
+				'sCepDestino'=>$nrzipcode,
+				'nVlPeso'=>$totals['vlweight'],
+				'nCdFormato'=>'1',
+				'nVlComprimento'=>$totals['vllength'],
+				'nVlAltura'=>$totals['vlheight'],
+				'nVlLargura'=>$totals['vlwidth'],
+				'nVlDiametro'=>'0',
+				'sCdMaoPropria'=>'S',
+				'nVlValorDeclarado'=>$totals['vlprice'],
+				'sCdAvisoRecebimento'=>'S'
+			]);
+			$xml = simplexml_load_file("http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx/CalcPrecoPrazo?".$qs);
+
+			$result = $xml->Servicos->cServico;
+
+			if ($result->MsgErro != '') {
+				Cart::setMsgError($result->MsgErro);
+			} else {
+				Cart::clearMsgError();
+			}
+
+			$this->setnrdays($result->PrazoEntrega);
+			$this->setvlfreight(Cart::formatValueToDecimal($result->Valor));
+			$this->setdeszipcode($nrzipcode);
+
+			$this->save();
+			return $result;
+		} else {
+
+		}
+	}
+
+	public static function formatValueToDecimal($value):float {
+		$value = str_replace('.', '', $value);
+		return str_replace(',', '.', $value);
+	}
+
+	public static function setMsgError()
+	{
+		$_SESSION[Cart::SESSION_ERROR] = $msg;
+	}
+
+	public static function getMsgError()
+	{
+		$msg = isset($_SESSION[Cart::SESSION_ERROR]) ? $_SESSION[Cart::SESSION_ERROR] : "";
+		Cart::clearMsgError();
+		return $msg;
+	}
+
+	public static function clearMsgError()
+	{
+		$_SESSION[Cart::SESSION_ERROR] = NULL;
+	}
+
+	public function updateFreight()
+	{
+		if ($this->getdeszipcode() != "") {
+			$this->setFreight($this->getdeszipcode());
+		}
+	}
+
+	public function getValues()
+	{
+		$this->getCalculateTotal();
+
+		return parent::getValues();
+	}
+
+	public function getCalculateTotal()
+	{
+		$this->updateFreight();
+
+		$totals = $this->getProductsTotals();
+
+		$this->setvlsubtotal($totals['vlprice']);
+		$this->setvltotal($totals['vlprice'] + $this->getvlfreight());
 	}
 
 }

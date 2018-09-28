672ff2d5df133b8dc9d8de8f5e13dc11f722a0b1
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2018-09-28 13:57:44 -0300

    Meus Pedidos - Resumo Carrinho

--- ecommerce/vendor/hcodebr/php-classes/src/Model/User.php
+++ ecommerce/vendor/hcodebr/php-classes/src/Model/User.php
@@ -357,6 +357,25 @@ class User extends Model {
 		]);
 	}
 
+	public function getOrders() {
+		$sql = new Sql();
+
+        $results = $sql->select("
+            select *
+            from tb_orders a
+            inner join tb_ordersstatus b using(idstatus)
+            inner join tb_carts c using(idcart)
+            inner join tb_users d on d.iduser = a.iduser
+            inner join tb_addresses e using(idaddress)
+            inner join tb_persons f on f.idperson = d.idperson
+            where a.iduser = :iduser
+        ", [
+            ':iduser'=>$this->getiduser()
+        ]);
+
+        return $results;
+	}
+
 }
 
  ?>
